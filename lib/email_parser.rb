75d52093aab879f2e6ee4de0ea7ea4db52bda7e7
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-10 00:58:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,11 @@ class EmailAddressParser
   attr_accessor :emails 
   
   def initialize(emails)
-    @emails = emails.split(' ')
+    @emails = emails
+  end
+  
+  def parse 
+    @emails.split(' ')
   end 
   
   
